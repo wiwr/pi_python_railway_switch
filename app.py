2faf69547cc9d7e2d7c4f9d1fa97cc88c7fbd15d
Witold Wróbel <wwrobel.mob@gmail.com>
2023-11-03 16:19:46 +0000

    fix issue with api calls

--- app.py
+++ app.py
@@ -8,24 +8,27 @@ servo_pins = [29 ,31 ,33 ,35 ,36 ,38 ,37 ,40]
 
 def move_servo(servo_id, angle):
     duty_cycle = 2.5 + 10 * angle / 180
-    servos[servo_id].ChangeDutyCycle(duty_cycle)
+    print(duty_cycle)
+    servos[servo_id].ChangeDutyCycle(angle)
+    sleep(1)
+    servos[servo_id].ChangeDutyCycle(0)
 
 @app.route("/")
 def index():
     print('index')
     return render_template('index.html')
 
-@app.route("/control", methods=['GET'])
+@app.route('/control', methods=['GET'])
 def control_servo():
     print('control')
     direction = request.args.get('direction') 
-    servo_id = int(request.args.get('servo_id'))
+    servo_id = int(request.args.get('servo_id')) - 1
     print(direction)
     print(servo_id)
     if direction == 'left':
-        move_servo(servo_id, 0) 
+        move_servo(servo_id, 2.5) 
     elif direction == 'right':
-        move_servo(servo_id, 180) 
+        move_servo(servo_id, 12.5) 
     return 'OK'
 
 GPIO.setmode(GPIO.BOARD)
@@ -33,10 +36,10 @@ GPIO.setmode(GPIO.BOARD)
 for pin in servo_pins:
     GPIO.setup(pin, GPIO.OUT)
 
-servos = [GPIO.PWM(pin, 50) for pin in servo_pins]
+servos = [GPIO.PWM(pin, 60) for pin in servo_pins]
 
 for servo in servos:
     servo.start(0)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
