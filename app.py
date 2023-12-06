1e680fbe1a63984ef8d996442fa75b192ffb7ca7
Witold Wróbel <wwrobel.mob@gmail.com>
2023-12-06 15:30:37 +0000

    better servo sync

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
-servo_pins = [29 ,31 ,33 ,35 ,36 ,38 ,37 ,40]
+servo_pins = [8, 31 ,33 ,35 ,36 ,38 ,37 ,40]
 
 def move_servo(servo_id, angle):
     duty_cycle = 2.5 + 10 * angle / 180
+    #duty_cycle = angle
     print(duty_cycle)
     servos[servo_id].ChangeDutyCycle(angle)
-    sleep(1)
+    sleep(0.5)
     servos[servo_id].ChangeDutyCycle(0)
 
 @app.route("/")
@@ -26,9 +27,9 @@ def control_servo():
     print(direction)
     print(servo_id)
     if direction == 'left':
-        move_servo(servo_id, 2.5) 
+        move_servo(servo_id, 2.5)
     elif direction == 'right':
-        move_servo(servo_id, 12.5) 
+        move_servo(servo_id, 6) 
     return 'OK'
 
 GPIO.setmode(GPIO.BOARD)
